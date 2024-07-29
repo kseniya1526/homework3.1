8ec57d8363b06ce474a4e1989072e61e2020149d
kseniya1526 <kseniak505@gmail.com>
2024-07-29 12:29:05 +0700

    Add files via upload

---
+++ homework3.py
@@ -0,0 +1,8 @@
+Name = 'Kseniya'
+print('Name:',Name, type(Name))
+Age = 21
+print('Age:',Age, type(Age))
+Age = (Age + 1)
+print('Age:',Age, type(Age))
+Is_student = True
+print('Is_student:',Is_student, type(Is_student))
